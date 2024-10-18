db2ad613f529d8984bf878032fb666964c03f7ec
evanhzg <evan.hoizey@ynov.com>
2024-10-18 23:00:31 +0200

    made data persistent through a Supabase db (postgre) and rendered data from given id

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,23 +1,79 @@
 "use client";
 
-import { useEditor, EditorContent } from "@tiptap/react";
+import { useEditor, EditorContent, JSONContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Focus from "@tiptap/extension-focus";
 import Highlight from "@tiptap/extension-highlight";
 import Paragraph from "@tiptap/extension-paragraph";
 import Document from "@tiptap/extension-document";
 import Placeholder from "@tiptap/extension-placeholder";
-
 import Text from "@tiptap/extension-text";
 import EditorMenuBar from "@/components/EditorMenuBar";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
 import "./TextEditor.css";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
+// Initialize Supabase client with environment variables
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+);
+
+// Custom document extension for tiptap editor
 const CustomDocument = Document.extend({
   content: "heading block*",
 });
 
-const TextEditor = ({ ...props }) => {
+const TextEditor = ({ documentId = "", ...props }: { documentId?: string }) => {
+  // State to manage loading state and initial content
+  const [loading, setLoading] = useState(true);
+  const [initialContent, setInitialContent] = useState<any | null>(null);
+  // State to manage the current document ID
+  const [currentDocumentId, setCurrentDocumentId] = useState<string | null>(
+    documentId
+  );
+
+  // Effect to fetch document content based on documentId
+  useEffect(() => {
+    const fetchDocument = async () => {
+      if (!documentId) {
+        setLoading(false);
+        return;
+      }
+
+      const { data, error } = await supabase
+        .from("notes")
+        .select("data")
+        .eq("id", documentId)
+        .single();
+
+      if (error) {
+        console.error("Error fetching document:", error);
+        setLoading(false);
+        return;
+      }
+
+      if (data) {
+        setInitialContent(data.data);
+      }
+
+      setLoading(false);
+    };
+
+    fetchDocument();
+  }, [documentId]);
+
+  // Initialize tiptap editor with custom configurations
   const editor = useEditor({
+    editorProps: {
+      attributes: {
+        class: "h-full",
+      },
+    },
+
+    immediatelyRender: false,
+
     extensions: [
       Focus.configure({
         className: "outline-none",
@@ -37,18 +93,54 @@ const TextEditor = ({ ...props }) => {
         },
       }),
       Highlight,
-      Paragraph,
-      Text,
     ],
-    content:
-      typeof window !== "undefined"
-        ? window.localStorage.getItem("editor-content")
-        : "",
-    onUpdate: ({ editor }) => {
-      window.localStorage.setItem("editor-content", editor.getHTML());
+
+    content: initialContent ? initialContent : "",
+
+    onUpdate: async ({ editor }) => {
+      const content = editor.getJSON();
+      if (!currentDocumentId) {
+        const { data, error } = await supabase
+          .from("notes")
+          .insert([{ data: content, name: "Untitled", author: "Anonymous" }])
+          .select("id")
+          .single();
+
+        if (error) {
+          console.error("Error creating document:", error);
+        } else {
+          setCurrentDocumentId(data.id);
+        }
+      } else {
+        const { error } = await supabase.from("notes").upsert([
+          {
+            id: currentDocumentId,
+            data: content,
+            name: "Untitled",
+            author: "Anonymous",
+          },
+        ]);
+
+        if (error) {
+          console.error("Error updating document:", error);
+        }
+      }
     },
   });
 
+  // Effect to set initial content in the editor
+  useEffect(() => {
+    if (initialContent && editor) {
+      editor.commands.setContent(initialContent);
+    }
+  }, [initialContent, editor]);
+
+  // Loading indicator
+  if (loading) {
+    return <Icon icon="line-md:loading-loop" />;
+  }
+
+  // Editor content and menu bar
   return (
     <div className="flex flex-col items-center bg-white rounded-xl w-[calc(100%-2rem)] lg:w-1/2 h-[25rem] overflow-hidden shadow-xl">
       <EditorContent
